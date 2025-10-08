cdead9e7a16d212369034ba72a214d561033cdf0
Winsy Lok <winsy.lok@team.telstra.com>
2025-10-08 11:47:52 +1100

    Took tutorials on Apple

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Winsy Lok on 8/10/2025.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Winsy Lok on 8/10/2025.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
