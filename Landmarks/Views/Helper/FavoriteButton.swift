9c2a8f70c75c9eaeed6ab56a97b4a65c1c6f69c5
Winsy Lok <winsy.lok@team.telstra.com>
2025-10-14 11:03:58 +1100

    .

---
+++ Landmarks/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Winsy Lok on 14/10/2025.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
+
